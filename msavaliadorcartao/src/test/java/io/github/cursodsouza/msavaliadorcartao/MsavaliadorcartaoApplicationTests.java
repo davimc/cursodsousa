4b404db3f9c5ebf0f42f277f1f8049c344e76534
davimc <davimatosc@hotmail.com>
2023-05-12 22:50:47 -0300

    Create microservice avaliador cartao

---
+++ msavaliadorcartao/src/test/java/io/github/cursodsouza/msavaliadorcartao/MsavaliadorcartaoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cursodsouza.msavaliadorcartao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsavaliadorcartaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
