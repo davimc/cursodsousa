5b1a4d6f2807a4416f21a08a904194070f8ee2ec
davimc <davimatosc@hotmail.com>
2023-05-17 17:39:26 -0300

    Fetching card data through the microservice

---
+++ msavaliadorcredito/src/main/java/io/github/cursodsouza/msavaliadorcredito/infra/client/CartoesResourceClient.java
@@ -0,0 +1,17 @@
+package io.github.cursodsouza.msavaliadorcredito.infra.client;
+
+import io.github.cursodsouza.msavaliadorcredito.model.CartaoCliente;
+import io.github.cursodsouza.msavaliadorcredito.model.DadosCliente;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(value = "mscartoes", path = "/cartoes")
+public interface CartoesResourceClient {
+
+    @GetMapping(params = "cpf")
+    ResponseEntity<List<CartaoCliente>> getCartoesByCpf(@RequestParam String cpf);
+}
--- msavaliadorcredito/src/main/java/io/github/cursodsouza/msavaliadorcredito/service/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/io/github/cursodsouza/msavaliadorcredito/service/AvaliadorCreditoService.java
@@ -1,22 +1,29 @@
 package io.github.cursodsouza.msavaliadorcredito.service;
 
+import io.github.cursodsouza.msavaliadorcredito.infra.client.CartoesResourceClient;
 import io.github.cursodsouza.msavaliadorcredito.infra.client.ClienteResourceClient;
+import io.github.cursodsouza.msavaliadorcredito.model.CartaoCliente;
 import io.github.cursodsouza.msavaliadorcredito.model.DadosCliente;
 import io.github.cursodsouza.msavaliadorcredito.model.SituacaoCliente;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clientesClient;
+    private final CartoesResourceClient cartoesClient;
     public SituacaoCliente obterSituacaoCliente(String cpf) {
         ResponseEntity<DadosCliente> dadosClienteResponseEntity = clientesClient.findCpf(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesResponseEntity = cartoesClient.getCartoesByCpf(cpf);
         return SituacaoCliente
                 .builder()
                 .cliente(dadosClienteResponseEntity.getBody())
+                .cartoesCliente(cartoesResponseEntity.getBody())
                 .build();
     }
 }
